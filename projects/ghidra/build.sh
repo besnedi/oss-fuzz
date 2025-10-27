5c679df705a0f443c8eaa516c89b3ea4f6813970
besnedi <besnedi@uwe.nsa.gov>
2025-10-27 13:23:00 -0400

    Update build.sh

--- projects/ghidra/build.sh
+++ projects/ghidra/build.sh
@@ -46,7 +46,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
   echo $fuzzer
   javac -cp $BUILD_CLASSPATH $fuzzer
   cp $SRC/$fuzzer_basename.class $OUT/
-
+  
   # Create an execution wrapper that executes Jazzer with the correct arguments.
   echo "#!/bin/bash
 # LLVMFuzzerTestOneInput for fuzzer detection.
@@ -56,13 +56,13 @@ if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
 else
   mem_settings='-Xmx2048m:-Xss1024k'
 fi
-LD_LIBRARY_PATH=\"$JVM_LD_LIBRARY_PATH\":\$this_dir \
+LD_LIBRARY_PATH=\"${JVM_LD_LIBRARY_PATH}\":\$this_dir \
 \$this_dir/jazzer_driver --agent_path=\$this_dir/jazzer_agent_deploy.jar \
---cp=$RUNTIME_CLASSPATH \
---target_class=$fuzzer_basename \
+--cp=${RUNTIME_CLASSPATH}:${OUT}/${fuzzer_basename}.class \
+--target_class=${fuzzer_basename} \
 --jvm_args=\"\$mem_settings:-Djava.awt.headless=true\" \
-\$@" > $OUT/$fuzzer_basename
-  chmod +x $OUT/$fuzzer_basename
+\$@" > ${OUT}/${fuzzer_basename}
+  chmod +x ${OUT}/${fuzzer_basename}
 
 done
 # e.g.
